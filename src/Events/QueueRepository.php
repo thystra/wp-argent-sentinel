48fd94a326473448f2fd284e6eaeacc7358c9059
Alan Johnson <alan@argentwolf.org>
2026-07-23 13:07:49 -0400

    correct site id

--- src/Events/QueueRepository.php
+++ src/Events/QueueRepository.php
@@ -5,6 +5,40 @@ namespace ArgentSentinel\WordPress\Events;
 use ArgentSentinel\WordPress\Database\Schema;
 
 final class QueueRepository {
+	/**
+	 * Explicit formats prevent WordPress's global wpdb field-name mappings
+	 * from coercing plugin-owned columns such as site_id.
+	 *
+	 * @var array<string,string>
+	 */
+	private const FIELD_FORMATS = array(
+		'event_uuid'        => '%s',
+		'batch_uuid'        => '%s',
+		'occurred_at_utc'   => '%s',
+		'recorded_at_utc'   => '%s',
+		'site_id'           => '%s',
+		'site_url'          => '%s',
+		'source_host'       => '%s',
+		'service'           => '%s',
+		'event_type'        => '%s',
+		'severity'          => '%s',
+		'outcome'           => '%s',
+		'source_ip'         => '%s',
+		'source_ip_version' => '%d',
+		'username'          => '%s',
+		'wordpress_user_id' => '%d',
+		'email_domain'      => '%s',
+		'email_identifier'  => '%s',
+		'user_agent'        => '%s',
+		'request_method'    => '%s',
+		'request_path'      => '%s',
+		'export_state'      => '%s',
+		'exported_at_utc'   => '%s',
+		'retry_count'       => '%d',
+		'last_export_error' => '%s',
+		'metadata_json'     => '%s',
+	);
+
 	/** @var object */
 	private $wpdb;
 
@@ -20,9 +54,21 @@ final class QueueRepository {
 	}
 
 	public function insert( Event $event ): bool {
+		$row = $event->toDatabaseRow();
+		$formats = array();
+
+		foreach ( array_keys( $row ) as $field ) {
+			if ( ! isset( self::FIELD_FORMATS[ $field ] ) ) {
+				return false;
+			}
+
+			$formats[] = self::FIELD_FORMATS[ $field ];
+		}
+
 		$result = $this->wpdb->insert(
 			$this->table_name,
-			$event->toDatabaseRow()
+			$row,
+			$formats
 		);
 
 		return false !== $result;
